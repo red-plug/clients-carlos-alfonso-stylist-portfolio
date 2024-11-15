ed114d7c1bdda4f5a68f86303b0219d23b523acc
andreslqr <me@andreslopez.com.mx>
2024-11-15 13:38:55 -0600

    brand colors for social media icons

--- components/SectionGallery.vue
+++ components/SectionGallery.vue
@@ -17,22 +17,98 @@
       </ul>
     </GalleryPhoto>
     <GalleryPhoto class="sm:row-span-2" label="Business Casual" photo="/images/business-casual-1.webp">
-      2
+      <p class="mt-4">
+        Si prefieres un look relajado y sin esfuerzo, el estilo casual es ideal para tu día a día. Perfecto para quienes quieren lucir bien, sentirse cómodos y estar siempre listos para cualquier plan.
+      </p>
+      <p class="font-bold mt-16">
+        Consejos:
+      </p>
+      <ul class="mt-4 list-disc	list-outside ps-4">
+        <li>
+          Elige piezas de algodón y mezclilla que sean cómodas y versátiles, como tus jeans favoritos o una camiseta básica.
+        </li>
+        <li>
+          Mantén colores fáciles de combinar para lograr un look fresco que no pase desapercibido. ¡Casual no significa aburrido!
+        </li>
+      </ul>
     </GalleryPhoto>
     <GalleryPhoto class="sm:row-span-3" label="Elegante" photo="/images/elegant-1.webp">
-      3
+      <p class="mt-4">
+        Perfecto para quienes buscan distinción y refinamiento en cada detalle. El estilo elegante habla de sofisticación y buen gusto. Ideal para eventos importantes o para quienes buscan una imagen siempre pulcra.
+      </p>
+      <p class="font-bold mt-16">
+        Consejos:
+      </p>
+      <ul class="mt-4 list-disc	list-outside ps-4">
+        <li>
+          Escoge prendas de cortes limpios y tejidos de alta calidad. Los trajes y vestidos bien ajustados serán siempre una apuesta segura.
+        </li>
+        <li>
+          Mantén los accesorios simples, pero impactantes; un buen reloj o un par de aretes delicados complementarán tu look sin robar protagonismo.
+        </li>
+      </ul>
     </GalleryPhoto>
     <GalleryPhoto class="sm:row-span-3 lg:col-start-1 lg:row-start-4" label="Romántico" photo="/images/romantic-1.webp">
-      4
+      <p class="mt-4">
+        Si amas los detalles delicados y los tonos suaves, ¡el estilo romántico está hecho para ti! Con encajes, volantes y colores pastel, lograrás un look soñador que destaca por su elegancia y femineidad.
+      </p>
+      <p class="font-bold bt-16">
+        Consejos:
+      </p>
+      <ul class="mt-4 list-disc	list-outside ps-4">
+        <li>
+          Incorpora vestidos y blusas con detalles sutiles, como encaje o estampados florales, para un toque encantador.
+        </li>
+        <li>
+          Los tonos pastel, como rosa o lavanda, le dan a tu outfit esa suavidad que hará que te sientas elegante y cómoda.
+        </li>
+      </ul>
     </GalleryPhoto>
     <GalleryPhoto class="sm:row-span-3 lg:col-start-2 lg:row-start-3" label="Dramático" photo="/images/drama-1.webp">
-      5
+      <p class="mt-4">
+        Si te encanta experimentar con tus outfits y ser el centro de atención, el estilo creativo es para ti. Aquí no hay reglas: mezcla estampados, colores vibrantes y texturas para reflejar tu personalidad única.
+      </p>
+      <p class="font-bold bt-16">
+        Consejos:
+      </p>
+      <ul class="mt-4 list-disc	list-outside ps-4">
+        <li>
+        </li>
+        <li>
+        </li>
+      </ul>
     </GalleryPhoto>
     <GalleryPhoto class="sm:row-span-3 lg:col-start-3 lg:row-start-4" label="Creativo" photo="/images/creative-1.webp">
-      6
+      <p class="mt-4">
+        Si te encanta experimentar con tus outfits y ser el centro de atención, el estilo creativo es para ti. Aquí no hay reglas: mezcla estampados, colores vibrantes y texturas para reflejar tu personalidad única.
+      </p>
+      <p class="font-bold bt-16">
+        Consejos:
+      </p>
+      <ul class="mt-4 list-disc	list-outside ps-4">
+        <li>
+          Atrévete a combinar patrones o colores que nunca hubieras imaginado juntos. ¡Tu estilo es una obra de arte!
+        </li>
+        <li>
+          Agrega accesorios llamativos y personales, como collares grandes o anillos coloridos, para mostrar tu lado más original.
+        </li>
+      </ul>
     </GalleryPhoto>
     <GalleryPhoto class="col-start-2 lg;row-start-6" label="Seductor" photo="/images/seductor-1.webp">
-      7
+      <p class="mt-4">
+        ¿Te gusta destacar y sentirte seguro de ti mismo? Con el estilo seductor puedes resaltar lo mejor de ti de una manera elegante. Prendas ajustadas y detalles de lujo harán que tu look sea simplemente irresistible.
+      </p>
+      <p class="font-bold bt-16">
+        Consejos:
+      </p>
+      <ul class="mt-4 list-disc	list-outside ps-4">
+        <li>
+          Prueba cortes que realcen tu figura y juega con detalles como transparencias o escotes sutiles. Menos es más.
+        </li>
+        <li>
+          Colores como el negro, rojo o burdeos harán que tu outfit tenga ese toque de sensualidad sin perder la clase.
+        </li>
+      </ul>
     </GalleryPhoto>
   </div>
 </template>
--- components/SidebarRight.vue
+++ components/SidebarRight.vue
@@ -1,16 +1,93 @@
 <template>
     <footer class="w-20 md:w-24 fixed right-0 top-0 h-screen flex flex-col justify-center items-center gap-y-4">
-        <Button as="a" target="_blank" rel="noopener" href="https://www.facebook.com/CarlosAlfonsoFashionStylist" size="small" severity="secondary" class="hover:text-primary hover:border-primary transition ease-in-out hover:scale-110">
+        <Button v-tooltip.left="facebook" as="a" target="_blank" rel="noopener" href="https://www.facebook.com/CarlosAlfonsoFashionStylist" size="small" severity="secondary" class="hover:text-primary hover:border-primary transition ease-in-out hover:scale-110">
             <Icon name="fa6-brands:facebook" />
         </Button>
-        <Button as="a" target="_blank" rel="noopener" href="https://www.tiktok.com/@carlosalfonso.stylist" size="small" severity="secondary" class="hover:text-primary hover:border-primary transition ease-in-out hover:scale-110">
+        <Button v-tooltip.left="tiktok" as="a" target="_blank" rel="noopener" href="https://www.tiktok.com/@carlosalfonso.stylist" size="small" severity="secondary" class="hover:text-primary hover:border-primary transition ease-in-out hover:scale-110">
             <Icon name="fa6-brands:tiktok" />
         </Button>
-        <Button as="a" target="_blank" rel="noopener" href="https://www.instagram.com/carlosalfonso_stylist" size="small" severity="secondary" class="hover:text-primary hover:border-primary transition ease-in-out hover:scale-110">
+        <Button v-tooltip.left="instagram" as="a" target="_blank" rel="noopener" href="https://www.instagram.com/carlosalfonso_stylist" size="small" severity="secondary" class="hover:text-primary hover:border-primary transition ease-in-out hover:scale-110">
             <Icon name="fa6-brands:instagram" />
         </Button>
-        <Button as="a" target="_blank" rel="noopener" href="https://wa.me/523331272308" size="small" severity="secondary" class="hover:text-primary hover:border-primary transition ease-in-out hover:scale-110">
+        <Button v-tooltip.left="whatsapp" as="a" target="_blank" rel="noopener" href="https://wa.me/523331272308" size="small" severity="secondary" class="hover:text-primary hover:border-primary transition ease-in-out hover:scale-110">
             <Icon name="fa6-brands:whatsapp" />
         </Button>
     </footer>
 </template>
+<script setup lang="ts">
+import type { TooltipOptions } from 'primevue/tooltip';
+
+const root = {
+    style: {
+        marginLeft: '-5px'
+    }
+}
+
+const facebook = {
+    value: 'Facebook',
+    pt: {
+        root,
+        arrow: {
+            style: {
+                borderLeftColor: 'rgb(var(--color-facebook))'
+            }
+        },
+        text: {
+            style: {
+                background: 'rgb(var(--color-facebook))'
+            }
+        }
+    },
+} as TooltipOptions
+
+const tiktok = {
+    value: 'TikTok',
+    pt: {
+        root,
+        arrow: {
+            style: {
+                borderLeftColor: 'rgb(var(--color-tiktok))'
+            }
+        },
+        text: {
+            style: {
+                background: 'rgb(var(--color-tiktok))',
+            }
+        }
+    },
+} as TooltipOptions
+
+const instagram = {
+    value: 'Instagram',
+    pt: {
+        root,
+        arrow: {
+            style: {
+                borderLeftColor: 'rgb(var(--color-instagram))'
+            }
+        },
+        text: {
+            style: {
+                background: 'rgb(var(--color-instagram))'
+            }
+        }
+    },
+} as TooltipOptions
+
+const whatsapp = {
+    value: 'WhatsApp',
+    pt: {
+        root,
+        arrow: {
+            style: {
+                borderLeftColor: 'rgb(var(--color-whatsapp))'
+            }
+        },
+        text: {
+            style: {
+                background: 'rgb(var(--color-whatsapp))'
+            }
+        }
+    },
+} as TooltipOptions
+</script>
