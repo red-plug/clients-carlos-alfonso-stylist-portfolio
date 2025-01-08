6758f609be500f8dd33e4f7d27669246494340af
andreslqr <me@andreslopez.com.mx>
2025-01-07 21:53:48 -0600

    fixed og image

--- pages/index.vue
+++ pages/index.vue
@@ -15,9 +15,10 @@
 <script setup lang="ts">
 useSeoMeta({
     title: 'Carlos Alfonso Stylist | Inicio',
-    ogTitle: 'Carlos Alfonso Stylist',
+    ogTitle: 'Carlos Alfonso Stylist',  
     description: 'Descubre cómo Carlos Alfonso Stylist transforma la moda en una experiencia única. Asesoría personalizada para encontrar tu esencia, romper reglas y proyectar tu mejor versión con seguridad y estilo. ¡Atrévete a reinventarte!',
-    ogDescription: 'Descubre cómo Carlos Alfonso Stylist transforma la moda en una experiencia única. Asesoría personalizada para encontrar tu esencia, romper reglas y proyectar tu mejor versión con seguridad y estilo. ¡Atrévete a reinventarte!',
-    ogImage: '/images/me-2.webp',
+    ogDescription: 'Asesoría personalizada para encontrar tu esencia, romper reglas y proyectar tu mejor versión con seguridad y estilo. ¡Atrévete a reinventarte!',
+    ogImage: '/images/me-3.jpg',
+    ogImageType: 'image/jpeg'
 })
 </script>
